4c923512d4854ffbd7cb494eb0a6f1a087563439
joemarney <marney.joe@gmail.com>
2024-10-15 18:55:53 +0100

    homework completed - total agility and strength mention

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,141 @@
-// src/App.jsx
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [strength, setStrength] = useState(0);
+  const [agility, setAgility] = useState(0);
+  const [zombieFighters, setZombieFighters] = useState([
+    {
+      name: "Survivor",
+      price: 12,
+      strength: 6,
+      agility: 4,
+      img: "https://via.placeholder.com/150/92c952",
+    },
+    {
+      name: "Scavenger",
+      price: 10,
+      strength: 5,
+      agility: 5,
+      img: "https://via.placeholder.com/150/771796",
+    },
+    {
+      name: "Shadow",
+      price: 18,
+      strength: 7,
+      agility: 8,
+      img: "https://via.placeholder.com/150/24f355",
+    },
+    {
+      name: "Tracker",
+      price: 14,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/d32776",
+    },
+    {
+      name: "Sharpshooter",
+      price: 20,
+      strength: 6,
+      agility: 8,
+      img: "https://via.placeholder.com/150/1ee8a4",
+    },
+    {
+      name: "Medic",
+      price: 15,
+      strength: 5,
+      agility: 7,
+      img: "https://via.placeholder.com/150/66b7d2",
+    },
+    {
+      name: "Engineer",
+      price: 16,
+      strength: 6,
+      agility: 5,
+      img: "https://via.placeholder.com/150/56acb2",
+    },
+    {
+      name: "Brawler",
+      price: 11,
+      strength: 8,
+      agility: 3,
+      img: "https://via.placeholder.com/150/8985dc",
+    },
+    {
+      name: "Infiltrator",
+      price: 17,
+      strength: 5,
+      agility: 9,
+      img: "https://via.placeholder.com/150/392537",
+    },
+    {
+      name: "Leader",
+      price: 22,
+      strength: 7,
+      agility: 6,
+      img: "https://via.placeholder.com/150/602b9e",
+    },
+  ]);
+
+  const handleAddFighter = (newMember) => {
+    const newTeam = [...team, newMember];
+    setTeam(newTeam);
+    setMoney(money - newMember.price);
+    setStrength(strength + newMember.strength);
+    setAgility(agility + newMember.agility);
+  };
+
+  const handleRemoveFighter = (removeMember) => {
+    const deleteMember = [...team];
+    setTeam(deleteMember.filter((a) => a.name !== removeMember.name));
+    setMoney(money + removeMember.price);
+    setStrength(strength - removeMember.strength);
+    setAgility(agility - removeMember.agility);
+  };
+
+  return (
+    <>
+      <h1>Zombie Fighters</h1>
+      <h2>Money: {money}</h2>
+      <h2>Team Strength: {strength}</h2>
+      <h2>Team Agility: {agility}</h2>
+      <h2>Team: </h2>
+      <p>{team.length <= 0 ? "You should pick some team members!" : null}</p>
+      <ul>
+        {team.map((member) => {
+          return (
+            <li key={member.name}>
+              <img src={member.img} />
+              <p>{member.name}</p>
+              <p>{member.price}</p>
+              <p>{member.strength}</p>
+              <p>{member.agility}</p>
+              <button onClick={() => handleRemoveFighter(member)}>Remove</button>
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Fighters:</h2>
+      <ul>
+        {zombieFighters.map((character) => {
+          return (
+            <li key={character.name} className={team.includes(character) ? "selected" : "notSelected"}>
+              <img src={character.img} />
+              <p>{character.name}</p>
+              <p>Price: {character.price}</p>
+              <p>Strength: {character.strength}</p>
+              <p>Agility: {character.agility}</p>
+              <button onClick={() => handleAddFighter(character)} disabled={character.price > money}>
+                Add
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default App;
